af41c0ef893ededb72fd67b82fe2faab9cb66b96
Lam <lthiet@student.ethz.ch>
2022-05-02 15:39:37 +0200

    v1

--- factory.py
+++ factory.py
@@ -1,42 +1,92 @@
+import uuid
 from bs4 import BeautifulSoup
 import requests
 import genanki
 import tempfile
+import os
 
-WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/Regions_of_France"
+WIKIPEDIA_URL = 'https://en.wikipedia.org'
+PAGE_NAME = "/wiki/Regions_of_France"
 MODEL = genanki.Model(
-    1980728030,  # Model ID
+    1943297000,  # Model ID
     "French Regions",  # Model name
     fields=[
         {"name": "Name"},
         {"name": "Capital"},
-        {"name": "Flag"},
         {"name": "Map"},
+        {"name": "Flag"},
     ],
     templates=[
         {
             "name": "Name / Capital",
-            "qfmt": "{{Name}}",
-            "afmt": "{{FrontSide}}<hr id=answer>{{Capital}}",
+            "qfmt": open("templates/name_capital/front.html", "r").read(),
+            "afmt": open("templates/name_capital/back.html", "r").read(),
+        },
+        {
+            "name": "Name / Map",
+            "qfmt": open("templates/name_map/front.html", "r").read(),
+            "afmt": open("templates/name_map/back.html", "r").read(),
+        },
+        {
+            "name": "Name / Flag",
+            "qfmt": open("templates/name_flag/front.html", "r").read(),
+            "afmt": open("templates/name_flag/back.html", "r").read(),
+        },
+        {
+            "name": "Capital / Name",
+            "qfmt": open("templates/capital_name/front.html", "r").read(),
+            "afmt": open("templates/capital_name/back.html", "r").read(),
+            
+        },
+        {
+            "name": "Map / Name",
+            "qfmt": open("templates/map_name/front.html", "r").read(),
+            "afmt": open("templates/map_name/back.html", "r").read(),
+
+        },
+        {
+            "name": "Flag / Name",
+            "qfmt": open("templates/flag_name/front.html", "r").read(),
+            "afmt": open("templates/flag_name/back.html", "r").read(),
         },
     ],
+    css=open("style.css", "r").read()
 )
+
 
 DECK = genanki.Deck(
     1997108613,  # Deck ID
     "French Regions",  # Deck name
 )
 
+PACKAGE = genanki.Package(DECK)
+
+
+def download_image(url):
+    id = uuid.uuid4()
+    headers = {
+            'User-Agent': 'anki_regions_of_france/0.0 (https://github.com/lthiet/anki_region_france, nguyenthiet.lam@gmail.com)'}
+    map_response = requests.get(url, headers=headers)
+
+    # Write the map to a file
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    map_download_path = os.path.join('data', str(id) + '.svg')
+    with open(map_download_path, 'wb') as f:
+        f.write(map_response.content)
+
+    # return os.path.abspath(map_download_path)
+    return map_download_path
 
 if __name__ == "__main__":
     # Get the HTML
-    page = requests.get(WIKIPEDIA_URL)
+    page = requests.get(WIKIPEDIA_URL + PAGE_NAME)
 
     # Parse the HTML
     soup = BeautifulSoup(page.text, "html.parser")
 
-    # Get the table
-    table = soup.find("table", {"class": "wikitable"})
+    # Get the third table
+    table = soup.find_all("table", {"class": "wikitable"})[2]
 
     # Transform the table into a list of lists
     table_rows = table.find_all("tr")
@@ -48,50 +98,60 @@ if __name__ == "__main__":
     # Remove the first row (header)
     table_data.pop(0)
 
+    media_files = []
     # Iterate over the table
-    for row in table_data:
+    for i, row in enumerate(table_data):
+        if i > 12:
+            continue
         # Get the name
-        name = row[2].get_text()
+        name = row[1].find('a').get_text().strip()
         # Get the capital
-        capital = row[4].get_text()
+        capital = row[3].get_text().strip()
 
-        # Get the flag URL
-        flag_url_tmp = row[0].find("a").get("href")
-        # Add the wiki link to it
-        flag_url_tmp = "https://en.wikipedia.org" + flag_url_tmp
-        # Request the page for the flag
-        flag_page = requests.get(flag_url_tmp)
-        # Parse the HTML
-        flag_soup = BeautifulSoup(flag_page.text, "html.parser")
-        # Find the div containing the flag, it has an id of "file"
-        flag_div = flag_soup.find("div", {"id": "file"})
-        # Get the URL of the flag
-        flag_url = "http:" + flag_div.find("a").get("href")
+        # Get the map
+        map_page_url = WIKIPEDIA_URL + row[-1].find('a').get('href')
+        map_page = requests.get(map_page_url)
+        map_soup = BeautifulSoup(map_page.text, "html.parser")
+        map_url = "https:" + \
+            map_soup.find('div', {"id": "file"}).find('a').get('href')
+        map_abs_path = download_image(map_url)
 
-        # Download the flag to a temporary file
-        flag_file = requests.get(flag_url, stream=True)
-        with tempfile.NamedTemporaryFile() as f:
-            for chunk in flag_file.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-            # Get the path of the file
-            flag_path = f.name
+        # Get the flag
+        region_page_url = row[1].find('a').get('href')
+        region_soup = BeautifulSoup(requests.get(WIKIPEDIA_URL + region_page_url).text, "html.parser")
+        flag_page_url_candidates = region_soup.find_all('a', {"class": "image"})
+        candidate_found = False
+        for candidate in flag_page_url_candidates:
+            if 'Flag' in candidate.get("href"):
+                flag_page_url = WIKIPEDIA_URL + candidate.get("href")
+                candidate_found = True
+                break
+        if not candidate_found:
+            flag_abs_path = None
+        else:
+            flag_page = requests.get(flag_page_url)
+            flag_soup = BeautifulSoup(flag_page.text, "html.parser")
+            flag_url = "https:" + flag_soup.find('div', {"id": "file"}).find('a').get('href')
+            flag_abs_path = download_image(flag_url)
+        
 
-        # # Put the flag into the media
-        # DECK.media_files = [
-        #     flag_path
-        # ]
+        # Put media files
+        media_files.append(map_abs_path)
+        if flag_abs_path:
+            media_files.append(flag_abs_path)
+
 
         note = genanki.Note(
             model=MODEL,
-            fields={
-                "Name": name,
-                "Capital": capital,
-                "Flag": name, # TODO: Add the flag
-                "Map": name # TODO: Add the map
-            }
+            fields=[
+                name,
+                capital,
+                f"<img src='{map_abs_path.split('/')[-1]}'>",
+                f"<img src='{flag_abs_path.split('/')[-1]}'>" if flag_abs_path else ""
+            ]
         )
         DECK.add_note(note)
-
-    # Save the deck
-    genanki.Package(DECK).write_to_file('output.apkg')
+    
+    # Write the package
+    PACKAGE.media_files = media_files
+    PACKAGE.write_to_file('output.apkg')
